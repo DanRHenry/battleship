d831505644e536594091a045e6b0a25a3b4c6a68
DanRHenry <danielrhenry@gmail.com>
2024-07-21 10:53:26 -0400

    investigating stopping broadcasting to self

--- scripts/backend.js
+++ scripts/backend.js
@@ -9,31 +9,44 @@ const PORT = 3400;
 
 const wss = new WebSocket.Server({ server: server });
 
-// let obj = { key0: "value0", key1: "value1" };
-// obj = obj;
-
 wss.on("connection", function connection(ws) {
-  console.log("Hello! A new client connected");
+  ws.on("error", console.error);
+
   ws.send("Welcome new client");
 
-  //   ws.on("message", (message, isBinary) => {
-  ws.on("message", (message, isBinary) => {
+    ws.on("message", (message, isBinary) => {
     // Broadcast the message to every connected client
-    wss.clients.forEach(function each(client) {
+    wss.clients.forEach(function each (client) {
       // Send to All
-      if (client.readyState === WebSocket.OPEN) {
+      if (client.readyState === WebSocket.OPEN && client !== ws) {
         // Send to All But Origin
-        if(client !== ws && client.readyState === WebSocket.OPEN) {
+        // console.log("client client client client client client client client :",client) 
+        // console.log('ws:',ws)
+        console.log("ws ws ws ws ws ws ws ws ws ws ws ws ws ws ws ws ws ws",ws)
+        if (client == ws) {
+          // console.log("same same same same same same same same same ")
+          return;
+          // {same: 'same'}
+          // client.send(message, { binary: isBinary });
+        }
+        else if(client != ws) {
+          console.log("different different different different different different different different ")
+          client.send(message, { binary: isBinary})
+        // if(client !== ws && client.readyState === WebSocket.OPEN) {
+
+
         // console.log(message);
         // client.send(message, { binary: isBinary });
 
         //! This will reference the commonly shared object
-        client.send("" + message)
+        // client.send(message, {binary: isBinary})
+        // client.send("" + message)
         // client.send(JSON.stringify(obj.key0), {binary: isBinary})
       }
     }
     });
   });
+
   ws.on("close", () => {
     console.log("connection closed");
   });
